6866383913ce33569f6394df380c6cc62e688946
fgsreally <1325041831@qq.com>
2023-03-02 21:27:26 +0800

    release:rc

--- packages/form/src/createModal.ts
+++ packages/form/src/createModal.ts
@@ -1,18 +1,7 @@
 import type { Component, DefineComponent, ExtractPropTypes } from 'vue'
 import { defineComponent, h, ref, render, shallowRef } from 'vue'
 
-// function createModelVnode(key: string, comp: Component) {
-//   if (!vm[key]) {
-//     const el = document.createElement('div')
-//     vm[key] = createVNode(comp) // 将组件渲染成虚拟节点
-//     // 这里需要将el 渲染到我们的页面中
-//     document.body.appendChild((render(vm[key], el), el)) // 渲染成真实节点扔到页面中
-//   }
-//   return vm[key]
-// }
-
-export function createModal(modalWrapper: Component, modelKey = 'modelValue',
-) {
+export const createModal = function (modalWrapper: Component, modelKey = 'modelValue') {
   let isMounted = false
   const isShow = ref(true)
   const content = shallowRef()
