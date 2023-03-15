0d3c20909fc216cd11573e3cb93bba6434dc2d4c
fgsreally <1325041831@qq.com>
2023-03-15 17:28:33 +0800

    feature:add metadata to prompt

--- packages/prompt/src/index.ts
+++ packages/prompt/src/index.ts
@@ -2,22 +2,40 @@
 import prompt from 'prompt'
 import { replacePromptStr } from './replacer'
 const EXPRESS_RE = /^{{(.*)}}$/
-export function fromJsonToData(jsonData: { [key: string]: any }) {
+export function fromJsonToData(jsonData: { [key: string]: any }, metadata?: any) {
   for (const i in jsonData) {
     if (typeof jsonData[i] === 'string' && EXPRESS_RE.test(jsonData[i])) {
       const expression = replacePromptStr(jsonData[i].match(EXPRESS_RE)[1])
-      const fn = new Function('v', 'prompt', `return ${expression}`)
-      jsonData[i] = (v: any) => fn(v, prompt)
+      const fn = new Function('v', 'prompt', 'meta', `console.log(meta);return ${expression}`)
+      jsonData[i] = (v: any) => fn(v, prompt, metadata)
     }
     if (typeof jsonData[i] === 'object')
-      fromJsonToData(jsonData[i])
+      fromJsonToData(jsonData[i], metadata)
   }
 }
 
-export async function createPrompt(data: { [key: string]: any }) {
+/**
+ * data is not immutable in `createPrompt`
+ *
+ * @param data JSON obj
+ * @param metadata any data inject to prompt
+ * @returns void
+ */
+export async function createPrompt(data: { [key: string]: any }, metadata?: any) {
   prompt.start()
-  fromJsonToData(data)
+  fromJsonToData(data, metadata)
   return prompt.get({ properties: data })
 }
 
 export { prompt }
+
+// example:
+// const data = {
+//   proxy: {
+//     description: 'Proxy url',
+//   },
+//   proxyCredentials: {
+//     description: 'Proxy credentials',
+//     ask: '{{<<proxy>>.value>0&&meta.value>10}}',
+//   },
+// }
