b119020b22bff36c1c73abc752f393fe698cfe5b
fgsreally <1325041831@qq.com>
2023-03-15 17:12:00 +0800

    feature:remove fetch in prompt

---
+++ .changeset/good-emus-perform.md
@@ -0,0 +1,5 @@
+---
+"phecda-core": patch
+---
+
+add more pipe and rule to core
--- packages/prompt/src/index.ts
+++ packages/prompt/src/index.ts
@@ -1,6 +1,5 @@
 /* eslint-disable no-new-func */
 import prompt from 'prompt'
-import fetch from 'node-fetch'
 import { replacePromptStr } from './replacer'
 const EXPRESS_RE = /^{{(.*)}}$/
 export function fromJsonToData(jsonData: { [key: string]: any }) {
@@ -15,24 +14,10 @@ export function fromJsonToData(jsonData: { [key: string]: any }) {
   }
 }
 
-//
-// Start the prompt
-//
-
-async function getRemoteConfig(url: string) {
-  const schema = await fetch(url)
-  const { data } = await schema.json() as { data: { [key: string]: any } }
-  return data
-}
-
 export async function createPrompt(data: { [key: string]: any }) {
   prompt.start()
   fromJsonToData(data)
   return prompt.get({ properties: data })
-}
-
-export async function createRemotePrompt(url: string) {
-  return await createPrompt(await getRemoteConfig(url))
 }
 
 export { prompt }
