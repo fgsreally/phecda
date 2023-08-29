48e0f9b8e04a5cad4bb8516591a104c30e9eb5aa
fgsreally <1325041831@qq.com>
2023-08-29 17:42:04 +0800

    feat(client): improve useC type

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -8,7 +8,24 @@ export function createBeacon(baseUrl: string) {
   }
 }
 
-export function useC<T extends new (...args: any) => any>(Module: T): InstanceType<T> {
+export type ExcludeNever<T> = { [K in keyof T as T[K] extends never ? never : K]: T[K] }
+
+export type PickJsonData<T> = ExcludeNever< T extends object | any[]
+  ? {
+      [K in keyof T]: T[K] extends ((...args: any[]) => any) | Symbol | undefined ? never : PickJsonData<T[K]>;
+    }
+  : T>
+
+export type GetFnFromObj<O> = Pick<O, {
+  [Key in keyof O]: O[Key] extends (...args: any) => any ? Key : never
+}[keyof O]>
+
+// exclude function/symbol/undefined in response obj,to make better type intelligence
+export type AsyncReturnToJson<T> = ExcludeNever<{
+  [Key in keyof T]: T[Key] extends (...args: any) => any ? (...args: Parameters<T[Key]>) => Promise<PickJsonData<Awaited< ReturnType<T[Key]>>>> : never
+}>
+
+export function useC<T extends new (...args: any) => any>(Module: T): AsyncReturnToJson<InstanceType<T>> {
   return new Module()
 }
 
