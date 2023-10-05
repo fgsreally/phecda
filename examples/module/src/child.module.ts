147af7f9fc90afcb64732341d61d54ad383dacb4
fgsreally <1325041831@qq.com>
2023-10-05 11:30:29 +0800

    chore: exit alpha mode

---
+++ examples/module/src/child.module.ts
@@ -0,0 +1,12 @@
+import { Tag } from 'phecda-module'
+@Tag('child')
+export class Child {
+  name = 'child1'
+  constructor() {
+    console.log('Child')
+  }
+
+  destroyed() {
+    console.log('Child destroy')
+  }
+}
---
+++ examples/module/src/main.ts
@@ -0,0 +1,5 @@
+import { Factory, moduleMap } from 'phecda-module'
+import { Parent } from './parent.module'
+
+Factory([Parent])
+window.a = moduleMap
