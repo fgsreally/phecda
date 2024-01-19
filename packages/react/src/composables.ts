7c77f5ed5932b5d12656763cc744298346c4805c
fgsreally <1325041831@qq.com>
2024-01-19 17:03:09 +0800

    init: phecda-react

---
+++ packages/react/src/composables.ts
@@ -0,0 +1,130 @@
+import { proxy, useSnapshot } from 'valtio'
+import { getHandler, getProperty, injectProperty, register } from 'phecda-core'
+
+import { createContext, createElement } from 'react'
+import mitt from 'mitt'
+import { wrapError } from './utils'
+import type { PhecdaEmitter, PhecdaInstance } from './types'
+
+export const emitter: PhecdaEmitter = mitt()
+
+let activePhecda: PhecdaInstance = {
+  useVMap: new WeakMap(),
+  useOMap: new WeakMap(),
+  useRMap: new WeakMap(),
+  fnMap: new WeakMap(),
+}
+
+export function setActivePhecda(phecda: PhecdaInstance) {
+  activePhecda = phecda
+}
+
+export function getActivePhecda() {
+  return activePhecda
+}
+
+export function useO<T extends new (...args: any) => any>(Model: T) {
+  const { useOMap } = getActivePhecda()
+
+  if (useOMap.has(Model))
+    return useOMap.get(Model)
+
+  const instance = new Model()
+
+  useOMap.set(Model, instance)
+  return instance
+}
+
+export function useR<T extends new (...args: any) => any>(Model: T): [InstanceType<T>, InstanceType<T>] {
+  const { useRMap, fnMap } = getActivePhecda()
+
+  if (useRMap.has(Model)) {
+    const proxyInstance = useRMap.get(Model)
+    return [useSnapshot(proxyInstance), proxyInstance]
+  }
+
+  const instance = useO(Model)
+
+  const proxyInstance = proxy(new Proxy(instance, {
+    get(target: any, key) {
+      if (typeof target[key] === 'function') {
+        if (fnMap.has(target[key]))
+          return fnMap.get(target[key])
+        const errorHandler = getHandler(target, key).find((item: any) => item.error)?.error
+        if (!errorHandler)
+          return target[key].bind(target)
+        const wrapper = wrapError(target, key, errorHandler)
+        fnMap.set(target[key], wrapper)
+        return wrapper
+      }
+
+      return target[key]
+    },
+    set(target: any, key, v) {
+      target[key] = v
+      return true
+    },
+  }))
+  register(proxyInstance)
+  useRMap.set(Model, proxyInstance)
+
+  return [useSnapshot(proxyInstance), proxyInstance]
+}
+
+export function createPhecdaContext() {
+  const { Provider } = createContext(null)
+  const eventRecord = [] as [string, (event: any) => void][]
+
+  return ({ children }: any) => {
+    // useEffect(() => {
+    //   return () => {
+    //     eventRecord.forEach(([eventName, handler]) =>
+    //       (emitter as any).off(eventName as any, handler),
+    //     )
+    //     eventRecord = []
+    //   }
+    // }, [])
+
+    if (!getProperty('watcher')) {
+      injectProperty('watcher', ({ eventName, instance, key, options }: { eventName: any; instance: any; key: string; options?: { once: boolean } }) => {
+        const fn = typeof instance[key] === 'function' ? instance[key].bind(instance) : (v: any) => instance[key] = v
+
+        if (options?.once) {
+          const handler = (...args: any) => {
+            fn(...args);
+            (emitter as any).off(eventName, handler)
+          }
+          (emitter as any).on(eventName, handler)
+          eventRecord.push([eventName, handler])
+        }
+        else {
+          eventRecord.push([eventName, fn]);
+          (emitter as any).on(eventName, fn)
+        }
+      })
+    }
+    if (!getProperty('storage')) {
+      injectProperty('storage', ({ tag, key, instance }: { instance: any; key: string; tag: string }) => {
+        if (!tag)
+          return
+        const initstr = localStorage.getItem(tag)
+        if (initstr) {
+          const data = JSON.parse(initstr)
+          if (key) {
+            instance[key] = data
+          }
+          else {
+            for (const i in data) {
+              if (i)
+                instance[i] = data[i]
+            }
+          }
+        }
+        globalThis.addEventListener('beforeunload', () => {
+          localStorage.setItem(tag, JSON.stringify(key ? instance[key] : instance))
+        })
+      })
+    }
+    return createElement(Provider, { value: null }, children)
+  }
+}
---
+++ packages/react/src/types.ts
@@ -0,0 +1,14 @@
+import type { Events } from 'phecda-core'
+
+export interface PhecdaEmitter {
+  on<N extends keyof Events>(eventName: N, cb: (args: Events[N]) => void): void
+  off<N extends keyof Events>(eventName: N, cb?: (args: Events[N]) => void): void
+  emit<N extends keyof Events>(eventName: N, param: Events[N]): void
+}
+
+export interface PhecdaInstance {
+  useVMap: WeakMap<any, any>
+  useOMap: WeakMap<any, any>
+  useRMap: WeakMap<any, any>
+  fnMap: WeakMap<any, any>
+}
