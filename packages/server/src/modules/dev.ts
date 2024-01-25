de9798447c49ecfe840d8dc645af1a8296a5fa4a
fgsreally <1325041831@qq.com>
2024-01-25 10:55:26 +0800

    feature(server): add config to Dev for modular

--- packages/server/src/modules/dev.ts
+++ packages/server/src/modules/dev.ts
@@ -2,13 +2,22 @@ import { Empty } from 'phecda-core'
 import { UNMOUNT_SYMBOL } from '../common'
 import { emitter } from '../core'
 
+
+
 @Empty
 export class Dev {
+  static config: any
   protected readonly emitter = emitter
   private readonly [UNMOUNT_SYMBOL]: (() => void)[] = []
-// work for hmr
-// exec callback  when module unmount
+  // work for hmr
+  // exec callback  when module unmount
   onUnmount(cb: () => void) {
     this[UNMOUNT_SYMBOL].push(cb)
   }
+
+}
+
+
+export function setModelConfig<T extends typeof Dev>(model: T, conf: T['config']) {
+  model.config = conf
 }
