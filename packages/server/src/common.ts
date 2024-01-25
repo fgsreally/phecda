5d2f76cd44df9be44aabb50d62becefc955686db
fgsreally <1325041831@qq.com>
2024-01-25 09:40:38 +0800

    feature(server): add PS_IMPORT_RE for frameworks based on ps

--- packages/server/src/common.ts
+++ packages/server/src/common.ts
@@ -7,3 +7,4 @@ export const IS_DEV = process.env.NODE_ENV === 'development'
 export const IS_STRICT = !!process.env.PS_STRICT
 export const ERROR_SYMBOL = '__PS_ERROR__'
 export const PS_FILE_RE = /\.(controller|service|module|extension|guard|interceptor|plugin|filter|pipe|edge)\.ts$/i
+export const PS_IMPORT_RE = /\.(controller|extension|guard|interceptor|plugin|filter|pipe|edge)\.ts$/i
