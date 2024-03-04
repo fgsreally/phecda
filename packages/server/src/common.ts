5f7eff0316d13472b51065040e7a0a4a42b9de06
fgsreally <1325041831@qq.com>
2024-03-04 15:57:41 +0800

    fix(server): support xx.ext.ts file hmr

--- packages/server/src/common.ts
+++ packages/server/src/common.ts
@@ -6,5 +6,7 @@ export const APP_SYMBOL = '__PS__'
 export const IS_DEV = process.env.NODE_ENV === 'development'
 export const IS_STRICT = !!process.env.PS_STRICT
 export const ERROR_SYMBOL = '__PS_ERROR__'
-export const PS_FILE_RE = /\.(controller|service|module|extension|guard|interceptor|plugin|filter|pipe|edge)\.ts$/i
-export const PS_IMPORT_RE = /\.(controller|extension|guard|interceptor|plugin|filter|pipe|edge)\.ts$/i
+export const PS_FILE_RE = /\.(controller|service|module|extension|ext|guard|interceptor|addon|filter|pipe|edge)\.ts$/i
+
+// ???
+export const PS_IMPORT_RE = /\.(controller|extension|ext|guard|interceptor|addon|filter|pipe|edge)\.ts$/i
