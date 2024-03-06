1bff9e04b91cfc4b3c23a870274bcb3c2a71e46e
fgsreally <1325041831@qq.com>
2024-03-06 18:33:33 +0800

    fix(server): add env PS_LOG_BAN to ban log

--- packages/server/src/common.ts
+++ packages/server/src/common.ts
@@ -5,6 +5,7 @@ export const META_SYMBOL = '__PS_META__'
 export const APP_SYMBOL = '__PS__'
 export const IS_DEV = process.env.NODE_ENV === 'development'
 export const IS_STRICT = !!process.env.PS_STRICT
+export const IS_LOG_BAN = !!process.env.PS_LOG_BAN
 export const ERROR_SYMBOL = '__PS_ERROR__'
 export const PS_FILE_RE = /\.(controller|service|module|extension|ext|guard|interceptor|addon|filter|pipe|edge)\.ts$/i
 
