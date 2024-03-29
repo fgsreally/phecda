d744e46a0076dab3f7e3dbe1c2ba31ee5a467c81
fgsreally <1325041831@qq.com>
2024-03-29 11:37:50 +0800

    fix(server): remove handler on Meta

--- packages/server/src/meta.ts
+++ packages/server/src/meta.ts
@@ -1,7 +1,7 @@
 import type { P } from './types'
 
 export class Meta {
-  constructor(public data: P.MetaData, public handlers: P.Handler[], public paramsType: any[]) {
+  constructor(public data: P.MetaData, public paramsType: any[]) {
 
   }
 }
