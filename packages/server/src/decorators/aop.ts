40535c91f2cba3984e7d54c96c4e6c6f6595a512
fgsreally <1325041831@qq.com>
2024-05-22 09:17:56 +0800

    refactor(server): refact all decorator

--- packages/server/src/decorators/aop.ts
+++ packages/server/src/decorators/aop.ts
@@ -1,100 +1,47 @@
-import { SHARE_KEY, getOwnState, setState, setStateKey } from 'phecda-core'
-
-export function Guard(...guards: string[]): any {
-  return (target: any, key?: PropertyKey) => {
-    if (!key)
-      key = SHARE_KEY
-    target = key === SHARE_KEY ? target.prototype : target
-
-    setStateKey(target, key)
-
-    const state = getOwnState(target, key)
-    if (!state.guards)
-      state.guards = []
-    state.guards.push(...guards)
-    setState(target, key, state)
+import { BaseParam } from './param'
+import { setPropertyState } from './utils'
+export function Guard(...guards: string[]): ClassDecorator | MethodDecorator {
+  return (target: any, k?: PropertyKey) => {
+    setPropertyState(target, k, (state) => {
+      if (!state.guards)
+        state.guards = []
+      state.guards.push(...guards)
+    })
   }
 }
 
-export function Plugin(...plugins: string[]): any {
-  return (target: any, key?: PropertyKey) => {
-    if (!key)
-      key = SHARE_KEY
-    target = key === SHARE_KEY ? target.prototype : target
-
-    setStateKey(target, key)
-
-    const state = getOwnState(target, key)
-    if (!state.plugins)
-      state.plugins = []
-    state.plugins.push(...plugins)
-    setState(target, key, state)
+export function Plugin(...plugins: string[]): ClassDecorator | MethodDecorator {
+  return (target: any, k?: PropertyKey) => {
+    setPropertyState(target, k, (state) => {
+      if (!state.plugins)
+        state.plugins = []
+      state.plugins.push(...plugins)
+    })
   }
 }
 
-export function Interceptor(...interceptors: string[]): any {
-  return (target: any, key?: PropertyKey) => {
-    if (!key)
-      key = SHARE_KEY
-    target = key === SHARE_KEY ? target.prototype : target
-
-    setStateKey(target, key)
-
-    const state = getOwnState(target, key)
-    if (!state.interceptors)
-      state.interceptors = []
-    state.interceptors.push(...interceptors)
-    setState(target, key, state)
+export function Interceptor(...interceptors: string[]): ClassDecorator | MethodDecorator {
+  return (target: any, k?: PropertyKey) => {
+    setPropertyState(target, k, (state) => {
+      if (!state.interceptors)
+        state.interceptors = []
+      state.interceptors.push(...interceptors)
+    })
   }
 }
-export function Filter(filter: string): any {
-  return (target: any, key?: PropertyKey) => {
-    if (!key)
-      key = SHARE_KEY
-    target = key === SHARE_KEY ? target.prototype : target
-
-    setStateKey(target, key)
-
-    const state = getOwnState(target, key)
-    state.filter = filter
-    setState(target, key, state)
+export function Filter(filter: string): ClassDecorator | MethodDecorator {
+  return (target: any, k?: PropertyKey) => {
+    setPropertyState(target, k, state => state.filter = filter)
   }
 }
-export function Pipe(key?: string) {
-  return (target: any, k?: PropertyKey, index?: any) => {
-    if (!k) {
-      k = SHARE_KEY
-      target = target.prototype
-
-      setStateKey(target, k)
-      const state = getOwnState(target, k)
-      state.pipe = key
-      setState(target, k, state)
-      return
-    }
-    if (typeof index !== 'number') {
-      setStateKey(target, k)
-      const state = getOwnState(target, k)
-      state.pipe = key
-      setState(target, k, state)
+export function Pipe(pipe?: string): ClassDecorator | MethodDecorator | ParameterDecorator {
+  return (target: any, k?: any, index?: any) => {
+    if (!k || typeof index !== 'number') {
+      setPropertyState(target, k, state => state.pipe = pipe)
 
       return
     }
 
-    setStateKey(target, k)
-
-    const state = getOwnState(target, k)
-
-    if (!state.params)
-      state.params = []
-
-    const existItem = state.params.find((item: any) => item.index === index)
-    if (existItem)
-      Object.assign(existItem, { pipe: key })
-
-    else
-      state.params.push({ pipe: key, index })
-
-    setState(target, k, state)
+    BaseParam({ pipe })(target, k, index)
   }
 }
