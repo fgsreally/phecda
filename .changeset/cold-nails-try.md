92ebfbdd2b6d40a221525b79c0b03105d8940a66
fgsreally <1325041831@qq.com>
2024-06-10 22:29:21 +0800

    feature(server): add setLogger

---
+++ .changeset/cold-nails-try.md
@@ -0,0 +1,5 @@
+---
+"phecda-server": patch
+---
+
+remove getConfig/setConfig; add setLogger
--- packages/server/src/utils.ts
+++ packages/server/src/utils.ts
@@ -1,9 +1,14 @@
 import pc from 'picocolors'
 import type { AbConstruct, Construct } from 'phecda-core'
-import { DataMap } from 'phecda-core'
 import { LOG_LEVEL } from './common'
 
 let time: number
+
+let internalLogger = console
+
+export function setLogger(logger: typeof console) {
+  internalLogger = logger
+}
 
 export function log(msg: string, level: 'error' | 'info' | 'warn' | 'log' = 'log') {
   const logLevel = {
@@ -21,30 +26,8 @@ export function log(msg: string, level: 'error' | 'info' | 'warn' | 'log' = 'log
   const current = Date.now()
   const interval = (time && current - time) ? `+${current - time}` : ''
   time = current
-  // eslint-disable-next-line no-console
-  console.log(`${pc.magenta('[phecda-server]')} ${pc.gray(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)} ${pc[color](msg)} ${pc.gray(interval)}`)
-}
 
-/**
- * @deprecated
- */
-export function getConfig<C = any>(key: string, defaultConf?: C): C {
-  if (!DataMap[key]) {
-    if (defaultConf)
-      return defaultConf
-    throw new Error(`config "${key}" is not existed`)
-  }
-
-  return DataMap[key]
-}
-/**
- * @deprecated
- */
-export function setConfig<C = any>(key: string, conf: C, force = true) {
-  if (DataMap[key] && !force)
-    return
-
-  DataMap[key] = conf
+  internalLogger[level](`${pc.magenta('[phecda-server]')} ${pc.gray(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)} ${pc[color](msg)} ${pc.gray(interval)}`)
 }
 
 export function Mix<C1 extends Construct | AbConstruct, C2 extends Construct>(InternalClass: C1, ExtendClass: C2) {
