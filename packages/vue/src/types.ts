d800aec7bd690d27218f0159940f458aabbd53bf
fgsreally <1325041831@qq.com>
2024-07-12 13:54:16 +0800

    types(vue): remove SchemaToObj

--- packages/vue/src/types.ts
+++ packages/vue/src/types.ts
@@ -1,8 +1,6 @@
 import type { Ref } from 'vue'
 
-// type ReadonlyValue<T> = {
-//   readonly [K in keyof T]: K extends 'value' ? T[K] : ReadonlyValue<T[K]>
-// }
+
 export declare const RawSymbol: unique symbol
 
 export type Raw<T> = T & {
@@ -14,7 +12,7 @@ export type ReplaceInstanceValues<I> = {
   [P in keyof I]: I[P] extends (...args: any[]) => any ? I[P] : I[P] extends Raw<infer O> ? O : Ref<I[P]>
 }
 
-export type SchemaToObj<S> = {
-  [P in keyof S]: S[P] extends object ? SchemaToObj<S[P]> : (S[P] extends string ? any : S[P]);
+// export type SchemaToObj<S> = {
+//   [P in keyof S]: S[P] extends object ? SchemaToObj<S[P]> : (S[P] extends string ? any : S[P]);
 
-}
+// }
