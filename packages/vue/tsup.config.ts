1c101d6258d67c2077b241732500be4ee4259f71
fgsreally <1325041831@qq.com>
2024-07-17 21:39:56 +0800

    revert(vue): only use devtools in development

--- packages/vue/tsup.config.ts
+++ packages/vue/tsup.config.ts
@@ -2,9 +2,7 @@ import type { Options } from 'tsup'
 
 export const tsup: Options = {
   entry: ['src/index.ts'],
-  define: {
-    'process.env.TEST': 'false',
-  },
+
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
