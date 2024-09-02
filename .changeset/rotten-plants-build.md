12f3d32933993bdc718b64b9e7a0f15600026322
fgsreally <1325041831@qq.com>
2024-09-02 11:38:33 +0800

    fix(vue): enhance lib mode

---
+++ .changeset/rotten-plants-build.md
@@ -0,0 +1,9 @@
+---
+"phecda-vue": patch
+---
+
+lib mode support 
+1. Init/Unmount
+2. params inject
+3. Reuse within the same component 
+4. force provide a new module
--- packages/vue/__test__/lib.spec.ts
+++ packages/vue/__test__/lib.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest'
 
 import { defineComponent } from 'vue'
 import { mount } from '@vue/test-utils'
-import { useIR, useIV } from '../src'
+import { Expose, Init, Unmount, useIR, useIV } from '../src'
 describe('in lib mode', () => {
   class User {
     age = 18
@@ -12,6 +12,13 @@ describe('in lib mode', () => {
     }
   }
 
+  @Expose
+  class Team {
+    name = 'A'
+    constructor(public user: User) {
+
+    }
+  }
   it('useIV', async () => {
     const Child = defineComponent({
       name: 'Child',
@@ -80,60 +87,119 @@ describe('in lib mode', () => {
     expect(wrapper.find('#child').text()).toBe('20')
   })
 
-  //   it('useR', async () => {
-  //     const Comp = defineComponent({
-  //       setup() {
-  //         class User {
-  //           name = ''
-  //           changeName() {
-  //             this.name = 'phecda-vue'
-  //           }
-  //         }
+  it('core decorators', () => {
+    let isMount = false
+    class InitUser extends User {
+      @Init
+      init() {
+        this.age = 20
+        isMount = true
+      }
 
-  //         const user = useR(User)
-  //         return { user }
-  //       },
-  //       template: '<div>{{user.name}}</div><button @click="user.changeName"></button>',
+      @Unmount
+      unmount() {
+        isMount = false
+      }
+    }
 
-  //     })
-  //     const wrapper = mount(Comp, {
-  //       global: {
-  //         plugins: [createPhecda()],
-  //       },
-  //     })
+    const wrapper = mount(defineComponent({
+      setup() {
+        const user = useIR(InitUser)
 
-  //     expect(wrapper.find('div').text()).toBe('')
-  //     await wrapper.find('button').trigger('click')
-  //     expect(wrapper.find('div').text()).toBe('phecda-vue')
-  //   })
+        return { user }
+      },
+      template: '<div id="container">{{user.age}}</div>',
 
-  //   it('usePhecda', async () => {
-  //     const Comp = defineComponent({
-  //       setup() {
-  //         class User {
-  //           name = ''
-  //           @Init
-  //           changeName() {
-  //             this.name = 'phecda-vue'
-  //           }
-  //         }
+    }))
 
-  //         const { reset } = usePhecda()
-  //         const user = useR(User)
-  //         return { user, reset, User }
-  //       },
-  //       template: '<div>{{user.name}}</div><button @click="reset(User)"></button>',
+    expect(wrapper.find('#container').text()).toBe('20')
 
-  //     })
-  //     const wrapper = mount(Comp, {
-  //       global: {
-  //         plugins: [createPhecda()],
-  //       },
-  //     })
+    wrapper.unmount()
 
-  //     // reset
-  //     expect(wrapper.find('div').text()).toBe('phecda-vue')
-  //     await wrapper.find('button').trigger('click')
-  //     expect(wrapper.find('div').text()).toBe('')
-  //   })
+    expect(isMount).toBeFalsy()
+  })
+
+  it('params provide/inject', async () => {
+    const wrapper = mount(defineComponent({
+
+      setup() {
+        const user = useIR(User)
+        const team = useIR(Team)
+        return { user, team }
+      },
+      template: '<div id="container">{{user.age}}</div><button @click="team.user.changeAge(20)"></button>',
+
+    }))
+
+    expect(wrapper.find('#container').text()).toBe('18')
+
+    await wrapper.find('button').trigger('click')
+    expect(wrapper.find('#container').text()).toBe('20')
+  })
+
+  it('force provide', async () => {
+    const Child1 = defineComponent({
+
+      setup() {
+        const user = useIR(User)// use User module from parent
+        const team = useIR(Team, true)
+
+        return { user, team }
+      },
+      template: '<div id="child">{{team.name}}--{{user.age}}</div>',
+
+    })
+
+    const Child2 = defineComponent({
+
+      setup() {
+        const user = useIR(User, true)
+        const team = useIR(Team, true)
+
+        return { user, team }
+      },
+      template: '<div id="child">{{team.name}}--{{user.age}}</div>',
+
+    })
+    const Parent = defineComponent({
+
+      setup() {
+        const user = useIR(User)
+        const team = useIR(Team)
+        function update() {
+          user.changeAge(20)
+          team.name = 'B'
+        }
+        return { user, team, update }
+      },
+      template: '<div id="parent">{{team.name}}--{{user.age}}</div><button @click="update"></button><slot></slot>',
+
+    })
+
+    const wrapper = mount(Parent, {
+      slots: {
+        default: Child1,
+      },
+    })
+    expect(wrapper.find('#parent').text()).toBe('A--18')
+    expect(wrapper.find('#child').text()).toBe('A--18')
+
+    await wrapper.find('button').trigger('click')
+
+    expect(wrapper.find('#parent').text()).toBe('B--20')
+    expect(wrapper.find('#child').text()).toBe('A--20')
+
+    const wrapper2 = mount(Parent, {
+      slots: {
+        default: Child2,
+      },
+    })
+    expect(wrapper2.find('#parent').text()).toBe('A--18')
+    expect(wrapper2.find('#child').text()).toBe('A--18')
+
+    await wrapper2.find('button').trigger('click')
+
+    expect(wrapper2.find('#parent').text()).toBe('B--20')
+    expect(wrapper2.find('#child').text()).toBe('A--18')
+  })
 })
