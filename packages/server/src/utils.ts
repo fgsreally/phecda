e8cd8f70c2b372c9ddbc552d1fe15f5b9af51749
fgsreally <1325041831@qq.com>
2024-09-23 14:15:27 +0800

    fix(server): setLogger can overwrite internal logger

--- packages/server/src/utils.ts
+++ packages/server/src/utils.ts
@@ -3,16 +3,24 @@ import { LOG_LEVEL } from './common'
 
 let time: number
 
-let internalLogger: Record<LogLevel, (msg: string) => void> = console
+let internalLogger: Record<LogLevel, (msg: string) => void>
 
-type LogLevel = 'error' | 'info' | 'warn' | 'log'
+type LogLevel = 'error' | 'info' | 'warn' | 'log' | 'debug'
 
 export function setLogger(logger: Record<LogLevel, (msg: string) => void>) {
   internalLogger = logger
 }
+export function getLogger() {
+  return internalLogger
+}
 
 export function log(msg: string, level: LogLevel = 'log') {
+
+  if (internalLogger) {
+    internalLogger[level](msg)
+  }
   const logLevel = {
+    debug: -1,
     info: 0,
     log: 1,
     warn: 2,
@@ -22,13 +30,13 @@ export function log(msg: string, level: LogLevel = 'log') {
   if (logLevel < LOG_LEVEL)
     return
 
-  const color = ({ error: 'red', info: 'gray', warn: 'yellow', log: 'green' } as const)[level]
+  const color = ({ debug: 'bgMagenta', error: 'red', info: 'gray', warn: 'yellow', log: 'green' } as const)[level]
   const date = new Date()
   const current = Date.now()
   const interval = (time && current - time) ? `+${current - time}` : ''
   time = current
 
-  internalLogger[level](`${pc.magenta('[phecda-server]')} ${pc.gray(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)} ${pc[color](msg)} ${pc.gray(interval)}`)
+  console[level](`${pc.magenta('[phecda-server]')} ${pc.gray(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`)} ${pc[color](msg)} ${pc.gray(interval)}`)
 }
 
 export { Mixin } from 'ts-mixer'
