e761625eaf1c33af99c59b0539e6e8ce1ed67f69
fgsreally <1325041831@qq.com>
2024-10-13 18:23:04 +0800

    feature(client): unplugin won't work/throw error when there is not config file

---
+++ .changeset/four-cups-pull.md
@@ -0,0 +1,7 @@
+---
+"phecda-client": minor
+---
+
+unplugin won't work and won't throw error when there is not ps.json(config file)
+
+server will restart when add/change config file(only in vite)
--- packages/client/src/unplugin.ts
+++ packages/client/src/unplugin.ts
@@ -1,5 +1,6 @@
 import { basename, dirname, isAbsolute, resolve } from 'path'
 import { createRequire } from 'module'
+import { existsSync } from 'fs'
 import { createUnplugin } from 'unplugin'
 import axios from 'axios'
 const require = createRequire(import.meta.url)
@@ -22,20 +23,35 @@ export default createUnplugin((options: {
 } = {}) => {
   const { configFile = './ps.json', port, interval = 3000, split = false, server = false } = options
   const psconfigPath = resolve(process.cwd(), configFile)
+
   const workdir = dirname(psconfigPath)
-  const config = require(psconfigPath)
+  const config: any = existsSync(psconfigPath) ? require(psconfigPath) : {}
   let command: string
 
   return {
-    name: 'phecda-server:client',
+    name: 'phecda-client',
     enforce: 'pre',
     vite: {
       configResolved(config) {
         command = config.command
       },
 
+      configureServer(server) {
+        // 监视指定文件
+        server.watcher.add(psconfigPath)
+
+        server.watcher.on('add', (path) => {
+          if (path === psconfigPath)
+            server.restart()
+        })
+        server.watcher.on('change', (path) => {
+          if (path === psconfigPath)
+            server.restart()
+        })
+      },
+
       buildStart() {
-        if (command !== 'serve') {
+        if (command !== 'serve' && config) {
           if (split && config.resolve) {
             config.resolve.forEach((item: any) => {
               if (item.filename) {
@@ -57,7 +73,7 @@ export default createUnplugin((options: {
 
     },
     resolveId(id, importer) {
-      if (!config.resolve || !importer || importer.includes('node_modules'))
+      if (!config?.resolve || !importer || importer.includes('node_modules'))
         return
       if (id.startsWith('.') || id.startsWith('/') || isAbsolute(id)) {
         const sourceMid = getFileMid(id)
