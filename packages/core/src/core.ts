e93be410233f7734994299ffc1068caa2b036c4b
fgsreally <1325041831@qq.com>
2025-07-28 17:20:50 +0800

    wip: update doc

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -28,6 +28,8 @@ export function init(proto: Phecda) {
     }
   }
 }
+
+
 export function getPhecdaFromTarget(target: any) {
   if (typeof target === 'function')
     return target.prototype// class/model
