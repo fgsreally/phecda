0ea165d447a0be950318f18fc176d020ec8ec2ad
fgsreally <1325041831@qq.com>
2025-12-28 18:01:26 +0800

    fix(server): aop modules can work with Mixin

--- packages/server/src/modules/addon.ts
+++ packages/server/src/modules/addon.ts
@@ -1,21 +1,21 @@
-import { getTag } from 'phecda-core'
-import { Context, addAddon } from '../context'
-import { ServerBase } from './base'
+import { getTag } from "phecda-core";
+import { Context, addAddon } from "../context";
+import { ServerBase } from "./base";
 
 export abstract class PAddon extends ServerBase {
-  readonly key: PropertyKey
+  readonly key: PropertyKey;
 
-  priority = 0
-  constructor(tag?: string) {
-    super()
-    this.key = tag || getTag(this)
+  priority = 0;
 
-    addAddon(this.key, this.use.bind(this), this.priority)
-
+  async init() {
+    await super.init();
+    //@ts-expect-error initialize
+    this.key = getTag(this);
+    addAddon(this.key, this.use.bind(this), this.priority);
     this.onUnmount(() => {
-      delete Context.addonRecord[this.key]
-    })
+      delete Context.addonRecord[this.key];
+    });
   }
 
-  abstract use(router: any, framework: string): undefined
+  abstract use(router: any, framework: string): undefined;
 }
--- packages/server/src/modules/guard.ts
+++ packages/server/src/modules/guard.ts
@@ -4,18 +4,20 @@ import type { BaseCtx } from '../types'
 import { ServerBase } from './base'
 
 export abstract class PGuard<Ctx extends BaseCtx = any> extends ServerBase {
-  abstract use(ctx: Ctx, next: () => Promise<any>): any
   readonly key: PropertyKey
+
   priority = 0
 
-  constructor(tag?: string) {
-    super()
-    this.key = tag || getTag(this)
-
+  async init() {
+    await super.init();
+    //@ts-expect-error initialize
+    this.key = getTag(this);
     addGuard(this.key, this.use.bind(this), this.priority)
-
     this.onUnmount(() => {
       delete Context.guardRecord[this.key]
     })
   }
+
+  abstract use(ctx: Ctx, next: () => Promise<any>): any
+
 }
